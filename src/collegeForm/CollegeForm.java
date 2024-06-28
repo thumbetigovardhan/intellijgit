1714f9b39e284e46b63a74a104f37d296a4b855f
T Govardhan <86399999+thumbetigovardhan@users.noreply.github.com>
2024-06-28 16:39:23 +0530

    Develop (#3)
    * commiting to git
    
    * commiting to git

---
+++ src/collegeForm/CollegeForm.java
@@ -0,0 +1,10 @@
+package collegeForm;
+
+public class CollegeForm {
+    String collegeStudentName = "Ram";
+
+    int rollNo = 1213;
+
+    int phNo = 63029;
+    String standard = "2nd year";
+}
---
+++ src/educationsociety/StudentForm.java
@@ -0,0 +1,8 @@
+package educationsociety;
+
+public class StudentForm {
+    String name = "Govardhan";
+    int  rollNo = 123;
+    int phNo = 998973;
+    String address = "Guntur";
+}
